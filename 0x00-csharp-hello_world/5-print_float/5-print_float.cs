80bbc18e228f92757b3e7702428764cc68c9ecd3
Dullahan <zabrak12@gmail.com>
2020-11-30 20:04:53 -0600

    changed 5 from rounding

--- 0x00-csharp-hello_world/5-print_float/5-print_float.cs
+++ 0x00-csharp-hello_world/5-print_float/5-print_float.cs
@@ -5,6 +5,6 @@ class Program
 	static void Main(string[] args)
         {
 		float number = 3.14159f;
-		Console.WriteLine("Float: {0}", Math.Round(number, 2));
+		Console.WriteLine("Float: {0:F2}", number);
         }
 }
