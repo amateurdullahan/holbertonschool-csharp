87ef8e8ff3da23a73d27bc8876955582507f5cfc
Dullahan <zabrak12@gmail.com>
2020-11-30 19:57:30 -0600

    9, 10, 11. 9 has an extraneous 0

--- 0x00-csharp-hello_world/11-concat_edges/11-concat_edges.cs
+++ 0x00-csharp-hello_world/11-concat_edges/11-concat_edges.cs
@@ -1,12 +1,11 @@
 ﻿using System;
 
-namespace _11_concat_edges
+class Program
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+	static void Main(string[] args)
+	{
+		string str = "C# (pronounced \"See Sharp\") is a simple, modern, object-oriented, and type-safe programming language. C# has its roots in the C family of languages and will be immediately familiar to C, C++, Java, and JavaScript programmers.";
+		str = str.Substring(49, 15) + str.Substring(2, 1) + str.Substring(80, 11) + str.Substring(2, 1) + str.Substring(119, 2) + str.Substring(2, 1) + str.Substring(0, 2);
+		Console.WriteLine(str);
         }
-    }
 }
--- 0x00-csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
+++ 0x00-csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
@@ -6,6 +6,7 @@ class Program
 	{
 		double percent = .7553;
 		double currency = 98765.4321;
-		// YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE.
+		Console.WriteLine("Percent: {0} tip", percent.ToString("P", System.Globalization.CultureInfo.CreateSpecificCulture("en-US.utf-8")));
+        Console.WriteLine("Currency: {0}", currency.ToString("C", System.Globalization.CultureInfo.CreateSpecificCulture("en-US.utf-8")));
 	}
 }
