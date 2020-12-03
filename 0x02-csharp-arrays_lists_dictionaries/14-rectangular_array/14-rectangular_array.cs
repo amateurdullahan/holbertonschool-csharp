89e0c6f1adc1d49a11a571fd22c0f38dcc7654b8
Dullahan <zabrak12@gmail.com>
2020-12-02 19:03:47 -0600

    working 12/13, base files for 14-16

---
+++ 0x02-csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _14_rectangular_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int w = 5;
+            int h = 5;
+            
+        }
+    }
+}
