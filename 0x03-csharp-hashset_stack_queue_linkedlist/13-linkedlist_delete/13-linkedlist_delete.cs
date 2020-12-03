3adfd98a56e105622c2d35f50b11facb5aa56517
Dullahan <zabrak12@gmail.com>
2020-12-03 16:08:47 -0600

    forgot to declare variable

--- 0x03-csharp-hashset_stack_queue_linkedlist/13-linkedlist_delete/13-linkedlist_delete.cs
+++ 0x03-csharp-hashset_stack_queue_linkedlist/13-linkedlist_delete/13-linkedlist_delete.cs
@@ -6,11 +6,12 @@ class LList
     public static void Delete(LinkedList<int> myLList, int index)
     {
         var node = myLList.First;
-        for (count = 0; count < index && count < myLList.Count; count++)
+        int cnt = 0;
+        for (cnt = 0; cnt < index && cnt < myLList.Count; cnt++)
         {
             node = node.Next;
         }
-        if (count == index)
+        if (cnt == index)
         {
             myLList.Remove(node);
         }
