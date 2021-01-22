47baf64f65c4f7c11025d384f323ece6bf6a4478
Dullahan <zabrak12@gmail.com>
2021-01-21 21:14:36 -0600

    forgot to push

---
+++ 0x09-csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+/// <summary>
+///  arithmancy
+/// </summary>
+class VectorMath
+{
+    public static double[] Add(double[] vector1, double[] vector2)
+    {
+        if (vector1.Length == 2 && vector2.Length == 2)
+        {
+            double[] vec = new double[] {0, 0};
+            vec[0] = Math.Round((vector1[0] + vector2[0]), 2);
+            vec[1] = Math.Round((vector1[1] + vector2[1]), 2);
+            return (vec);
+        }
+        else if (vector1.Length == 3 && vector2.Length == 3)
+        {
+            double[] vec = new double[] {0, 0, 0};
+            vec[0] = Math.Round((vector1[0] + vector2[0]), 2);
+            vec[1] = Math.Round((vector1[1] + vector2[1]), 2);
+            vec[2] = Math.Round((vector1[2] + vector2[2]), 2);
+            return (vec);
+        }
+        else
+        {
+            double[] vec = new double[] {-1};
+            return (vec);
+        }
+    }
+}
