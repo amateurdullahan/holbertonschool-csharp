7ab48bb5a3ddd5887d2fc5aba7a096af069dce79
Dylan Carey <dylan.carey@live.com>
2021-04-13 18:40:27 -0500

    tasks 0-12

---
+++ 0x01-csharp-ifelse_loops_methods/11-print_diagonal/11-print_diagonal.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Line
+{
+    public static void PrintDiagonal(int length)
+    {
+        for (int i = 0; i < length; i++)
+        {
+            for (int s = 0; s < i; s++) 
+            {
+                Console.Write(" ");
+            }
+            Console.WriteLine("\\");
+        }
+        Console.WriteLine();
+    }
+}
---
+++ 0x01-csharp-ifelse_loops_methods/8-print_last_digit/8-print_last_digit.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Number
+{
+    public static int PrintLastDigit(int number)
+    {
+        int last_num = Math.Abs(number % 10);
+        Console.Write(last_num);
+        return last_num;
+    }
+}
