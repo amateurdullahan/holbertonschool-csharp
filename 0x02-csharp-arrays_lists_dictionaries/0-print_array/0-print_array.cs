7341358897dbf8d9cd28ca2035c43ac9b14c6c9d
Dylan Carey <dylan.carey@live.com>
2021-04-14 20:53:11 -0500

    task 0-16

---
+++ 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Array
+{
+    public static int[] CreatePrint(int size)
+    {
+        if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+        if (size == 0)
+        {
+            int[] emptyArray = new int[0];
+            Console.Write("\n");
+            return emptyArray;
+        }
+        int[] newArray = new int[size];
+        int i;
+        for (i = 0; i < size; i++)
+        {
+            newArray[i] = i;
+            Console.Write(i);
+            if (i == (size -1))
+            {
+                Console.Write("\n");
+            }
+            else
+            {
+                Console.Write(" ");
+            }
+        }
+        return newArray;
+    }
+}
---
+++ 0x02-csharp-arrays_lists_dictionaries/100-jagged_array/100-jagged_array.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[][] arr = new int[3][];
+        arr[0] = new int[] {0, 1, 2, 3};
+        arr[1] = new int[] {0, 1, 2, 3, 4, 5, 6};
+        arr[2] = new int[] {0, 1,};
+
+        for (int y = 0; y < arr.GetLength(0); y++) {
+            for (int x = 0; x < arr[y].Length; x++) {
+                Console.Write("{0:D}", arr[y][x]);
+                if (x != arr[y].Length - 1) {
+                    Console.Write(" ");
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
