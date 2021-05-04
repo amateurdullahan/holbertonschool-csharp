5ba5f5ab8592d8a6372cb79ec57ad5930d77115c
Dylan Carey <dylan.carey@live.com>
2021-05-03 21:51:29 -0500

    tasks 5-9

---
+++ 0x08-csharp-inheritance/5-shape/5-shape.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+class Shape
+{
+    /// <summary> shape of what to come /// </summary>
+    public virtual int Area()
+    {
+        throw new NotImplementedException("Area() is not implemented");
+    }
+}
---
+++ 0x08-csharp-inheritance/8-shape/8-shape.cs
@@ -0,0 +1,88 @@
+﻿using System;
+
+class Shape
+{
+    /// <summary> shape of what to come /// </summary>
+    public virtual int Area()
+    {
+        throw new NotImplementedException("Area() is not implemented");
+    }
+}
+
+class Rectangle : Shape
+{
+    /// <summary> rectangle shape /// </summary>
+    private int width;
+    public int Width
+    {
+        get
+        {
+            return (width);
+        }
+        set
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Width must be greater than or equal to 0");
+            }
+            else
+            {
+                width = value;
+            }
+        }
+    }
+    private int height;
+    public int Height
+    {
+        get
+        {
+            return (height);
+        }
+        set
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Height must be greater than or equal to 0");
+            }
+            else
+            {
+                height = value;
+            }
+        }
+    }
+    public new int Area()
+    {
+        return (height * width);
+    }
+    public override string ToString()
+    {
+        string str = "[Rectangle] " + width + " / " + height;
+        return (str);    
+    }
+}
+
+class Square : Rectangle
+{
+    /// <summary> square ///</summary>
+    private int size;
+    public int Size
+    {
+        get
+        {
+            return (size);
+        }
+        set
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Size must be greater than or equal to 0");
+            }
+            else
+            {
+                size = value;
+                Height = value;
+                Width = value;
+            }
+        }
+    }
+}
