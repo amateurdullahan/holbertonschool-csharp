07056c7951e4020b5d72493da2f209e017d75b69
Dylan Carey <dylan.carey@live.com>
2021-05-03 21:13:22 -0500

    task 0 + 1

---
+++ 0x08-csharp-inheritance/1-instance/1-instance.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+/// <summary> utility functions </summary>
+class Obj
+{
+    /// <summary> checks if array </summary>
+    public static bool IsInstanceOfArray(object obj)
+    {
+        if (obj.GetType().IsArray == true)
+        {
+            return true;
+        }
+        return false;
+    }
+}
