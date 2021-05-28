dc2121e63e5b6e8ddfd0f91fef5da8c4b0315147
Dylan Carey <dylan.carey@live.com>
2021-05-27 22:16:19 -0500

    last third plus xml

---
+++ 0x09-csharp-linear_algebra/28-determinant/28-determinant.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+/// <summary> function for matrix math </summary>
+class MatrixMath
+{
+    /// <summary> calculate determinant of matrix </summary>
+    public static double Determinant(double[,] matrix)
+    {
+        if (matrix.GetLength(0) == 2 && matrix.GetLength(1) == 2)
+        {
+            return Math.Round(matrix[0,0] * matrix[1,1] - matrix[0,1] * matrix[1,0], 2);
+        }
+
+        if (matrix.GetLength(0) == 3 && matrix.GetLength(1) == 3)
+        {
+            return Math.Round(matrix[0,0] * (matrix[1,1] * matrix[2,2] - matrix[1,2] * matrix[2,1])
+                    - matrix[0,1] * (matrix[1,0] * matrix[1,1] - matrix[1,2] * matrix[2,0])
+                    + matrix[0,2] * (matrix[1,0] * matrix[2,1] - matrix[1,1] * matrix[2,0]), 2);
+        }
+        return -1;
+    }
+}
---
+++ 0x09-csharp-linear_algebra/33-inverse_2D/33-inverse_2D.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+/// <summary> function for matrix math </summary>
+class MatrixMath
+{
+    /// <summary> calculate inverse of matrix </summary>
+    public static double[,] Inverse2D(double[,] matrix)
+    {
+        if (matrix.GetLength(0) != 2 || matrix.GetLength(1) != 2)
+        {
+            return new double[,] {{-1}};
+        }
+
+        double det = matrix[0,0] * matrix[1,1] - matrix[0,1] * matrix[1,0];
+
+        if (det == 0)
+        {
+            return new double[,] {{-1}};
+        }
+
+        return new double[,] {{Math.Round(matrix[1,1] / det, 2), Math.Round(-matrix[0,1] / det, 2)}, {Math.Round(-matrix[1,0] / det, 2), Math.Round(matrix[0,0] / det, 2)}};
+    }
+}
