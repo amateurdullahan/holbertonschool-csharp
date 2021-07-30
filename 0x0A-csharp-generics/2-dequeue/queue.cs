88f2b95bfd228ab1616ebe05ae3919b1b7707342
Dylan Carey <dylan.carey@live.com>
2021-07-30 10:30:41 -0500

    worked on during pld

---
+++ 0x0A-csharp-generics/2-dequeue/queue.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+class Queue<T>
+{
+    public Node head;
+    public Node tail;
+    public int count;
+
+    public Type CheckType()
+    {
+        return (typeof(T));
+    }
+
+    public void Enqueue(T value)
+    {
+        if (count == 0)
+        {
+            head = new Node(value);
+            tail = head;
+        } else
+        {
+            tail.next = new Node(value);
+            tail = tail.next;
+        }
+        count++;
+    }
+
+    public T Dequeue()
+    {
+        if (count == 0)
+        {
+            Console.WriteLine("Queue is empty");
+            return default(T);
+        }
+        T value = head.value;
+        head = head.next;
+        count--;
+        return (value);
+    }
+
+    public class Node
+    {
+        public T value = default(T);
+        public Node next;
+
+        public Node (T Value)
+        {
+            this.value = value;
+        }
+    }
+
+    public int Count()
+    {
+        return (count);
+    }
+}
---
+++ 0x0A-csharp-generics/4-print/queue.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+class Queue<T>
+{
+    public Node head;
+    public Node tail;
+    public int count;
+
+    public Type CheckType()
+    {
+        return (typeof(T));
+    }
+
+    public void Enqueue(T value)
+    {
+        if (count == 0)
+        {
+            head = new Node(value);
+            tail = head;
+        } else
+        {
+            tail.next = new Node(value);
+            tail = tail.next;
+        }
+        count++;
+    }
+
+    public T Dequeue()
+    {
+        if (count == 0)
+        {
+            Console.WriteLine("Queue is empty");
+            return default(T);
+        }
+        T value = head.value;
+        head = head.next;
+        count--;
+        return (value);
+    }
+
+    public class Node
+    {
+        public T value = default(T);
+        public Node next;
+
+        public Node (T newValue)
+        {
+            this.value = newValue;
+        }
+    }
+
+    public int Count()
+    {
+        return (count);
+    }
+
+    public T Peek()
+    {
+        if (head == null)
+        {
+            Console.WriteLine("Queue is empty");
+            return default(T);
+        }
+        return (head.value);
+    }
+    
+    public void Print()
+    {
+        if (head == null)
+        {
+            Console.WriteLine("Queue is empty");
+        }
+        else
+        {
+            Node temp = head;
+            while (temp != null)
+            {
+                Console.WriteLine(temp.value);
+                temp = temp.next;
+            }
+        }
+    }
+}
