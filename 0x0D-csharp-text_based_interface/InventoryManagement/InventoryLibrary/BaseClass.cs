429ab4a74d910030bcac7a991b2dd11e0f899f37
Dylan Carey <dylan.carey@live.com>
2021-08-05 12:13:19 -0500

    Task 0

---
+++ 0x0D-csharp-text_based_interface/InventoryManagement/InventoryLibrary/BaseClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class BaseClass
+{
+    string id;
+    DateTime date_created;
+    DateTime date_updated;
+
+    public BaseClass()
+    {
+        this.id = System.Guid.NewGuid().ToString();
+        this.date_created = this.date_updated = DateTime.Now;
+    }  
+    
+}
