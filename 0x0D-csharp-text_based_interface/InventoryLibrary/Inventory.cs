2dfacde2ba22ffe84624f9e05a94c3111604f1da
Dylan Carey <dylan.carey@live.com>
2021-08-08 15:47:00 -0500

    Missing files

---
+++ 0x0D-csharp-text_based_interface/InventoryLibrary/Inventory.cs
@@ -0,0 +1,18 @@
+using System;
+
+class Inventory : BaseClass
+{
+    string user_id;
+    string item_id;
+    int quantity;
+
+    public Inventory(string user_id, string item_id, int quantity)
+    {
+        this.user_id = user_id;
+        this.item_id = item_id;
+        if (quantity < 0)
+            this.quantity = 1;
+        else
+            this.quantity = quantity;
+    }
+}
---
+++ 0x0D-csharp-text_based_interface/InventoryLibrary/User.cs
@@ -0,0 +1,11 @@
+using System;
+
+class User : BaseClass
+{
+    string name;
+
+    public User(string name)
+    {
+        this.name = name;
+    }
+}
